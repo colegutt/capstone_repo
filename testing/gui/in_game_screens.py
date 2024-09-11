fdc9fa8d3bf3203563de66c94f3b0f345833de4a
colegutt <cole.gutterman@uky.edu>
2024-09-11 11:23:59 -0400

    added buttons on game over

--- testing/gui/in_game_screens.py
+++ testing/gui/in_game_screens.py
@@ -2,8 +2,6 @@ from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 from memory_game import MemoryGame
 
-from PyQt5.QtCore import pyqtSignal, QThread
-
 class GameThread(QThread):
     score_updated = pyqtSignal(int)
     game_over = pyqtSignal()  # Add a signal for game over
@@ -25,13 +23,6 @@ class GameThread(QThread):
         if self.memory_game:
             self.memory_game.stop()  # Stop the game if running
 
-    
-    def stop(self):
-        if self.memory_game:
-            self.memory_game.stop()  # Stop the game if running
-
-
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
 class MemoryInGameScreen(QWidget):
     def __init__(self, stacked_widget, app_init):
@@ -48,15 +39,11 @@ class MemoryInGameScreen(QWidget):
             self.set_title(),
             self.create_score_label(),
             self.create_pause_button(),
-            self.create_game_over_label()
+            self.create_game_over_label(),
+            self.create_play_again_button(),
+            self.create_go_back_button()
         )
-
-    def create_game_over_label(self):
-        self.game_over_label = QLabel(f'GAME OVER!', self)
-        self.game_over_label.setStyleSheet("color: red; font-size: 68px; font-weight: bold;")
-        self.game_over_label.setAlignment(Qt.AlignCenter)
-        self.game_over_label.setVisible(False)  # Initially hidden
-        return self.game_over_label
+        self.hide_end_game_buttons()  # Initially hide the end game buttons
 
     def set_title(self):
         title = QLabel('Memory', self)
@@ -87,7 +74,44 @@ class MemoryInGameScreen(QWidget):
         pause_button.clicked.connect(self.pause_game)
         return pause_button
 
-    def set_layout(self, title, score_label, pause_button, game_over_label):
+    def create_game_over_label(self):
+        self.game_over_label = QLabel(f'GAME OVER!', self)
+        self.game_over_label.setStyleSheet("color: red; font-size: 40px; font-weight: bold;")
+        self.game_over_label.setAlignment(Qt.AlignCenter)
+        self.game_over_label.setVisible(False)  # Initially hidden
+        return self.game_over_label
+
+    def create_play_again_button(self):
+        self.play_again_button = QPushButton('Play Again', self)
+        self.play_again_button.setStyleSheet("""
+            background-color: green; 
+            color: white; 
+            border-radius: 10px; 
+            font-size: 24px; 
+            font-weight: bold;
+            width: 300px;
+            height: 75px;
+        """)
+        self.play_again_button.clicked.connect(self.reset_game)
+        self.play_again_button.setVisible(False)  # Initially hidden
+        return self.play_again_button
+
+    def create_go_back_button(self):
+        self.go_back_button = QPushButton('Go Back', self)
+        self.go_back_button.setStyleSheet("""
+            background-color: red; 
+            color: white; 
+            border-radius: 10px; 
+            font-size: 24px; 
+            font-weight: bold;
+            width: 300px;
+            height: 75px;
+        """)
+        self.go_back_button.clicked.connect(self.select_new_game)
+        self.go_back_button.setVisible(False)  # Initially hidden
+        return self.go_back_button
+
+    def set_layout(self, title, score_label, pause_button, game_over_label, play_again_button, go_back_button):
         main_layout = QVBoxLayout()
         top_layout = QHBoxLayout()
         top_layout.addWidget(pause_button, alignment=Qt.AlignLeft)
@@ -96,8 +120,11 @@ class MemoryInGameScreen(QWidget):
         main_layout.addWidget(title)
         main_layout.addStretch()
         main_layout.addWidget(score_label, alignment=Qt.AlignCenter)
-        main_layout.addStretch()
+        main_layout.addSpacing(10)
         main_layout.addWidget(game_over_label, alignment=Qt.AlignCenter)
+        main_layout.addSpacing(10)
+        main_layout.addWidget(play_again_button, alignment=Qt.AlignCenter)
+        main_layout.addWidget(go_back_button, alignment=Qt.AlignCenter)
         main_layout.addStretch()
         self.setLayout(main_layout)
     
@@ -105,12 +132,14 @@ class MemoryInGameScreen(QWidget):
         if self.game_thread is None or not self.game_thread.isRunning():
             self.game_thread = GameThread()
             self.game_thread.score_updated.connect(self.update_score_from_game)
-            self.game_thread.game_over.connect(self.show_game_over_label)  # Connect game over signal
+            self.game_thread.game_over.connect(self.show_end_game_buttons)  # Connect game over signal
             self.game_thread.start()
         self.game_over_label.setVisible(False)  # Hide the label when the game starts
 
-    def show_game_over_label(self):
-        self.game_over_label.setVisible(True)  # Show the "GAME OVER!" label when the game ends
+    def show_end_game_buttons(self):
+        self.game_over_label.setVisible(True) 
+        self.play_again_button.setVisible(True) 
+        self.go_back_button.setVisible(True)  
     
     def update_score_from_game(self, num_round):
         self.score = num_round
@@ -132,6 +161,7 @@ class MemoryInGameScreen(QWidget):
             self.game_thread.memory_game.stop() 
             self.game_thread.quit()
             self.game_thread.wait()
+        self.show_end_game_buttons()  # Ensure end game buttons are shown
     
     def save_high_score(self):
         if self.app_init.memory_hs < self.score:
@@ -144,3 +174,13 @@ class MemoryInGameScreen(QWidget):
         self.save_high_score()
         self.score = 0
         self.score_label.setText(f'Score: {self.score}')
+        self.hide_end_game_buttons()
+        self.start_game()
+    
+    def select_new_game(self):
+        self.stacked_widget.setCurrentIndex(1)
+
+    def hide_end_game_buttons(self):
+        self.game_over_label.setVisible(False)
+        self.play_again_button.setVisible(False)
+        self.go_back_button.setVisible(False)
