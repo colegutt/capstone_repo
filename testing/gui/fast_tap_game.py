1a6a052dbb41f413e78e47d8297d6f0c20f3e76a
colegutt <cole.gutterman@uky.edu>
2024-09-25 11:46:40 -0400

    Checkpoint

--- testing/gui/fast_tap_game.py
+++ testing/gui/fast_tap_game.py
@@ -6,7 +6,7 @@ from general_functions import GeneralFunctions
 from bluetooth import BluetoothSocket, BluetoothError, RFCOMM
 
 # Game Parameters
-GAME_RUN_TIME = 30
+GAME_RUN_TIME = 5
 SPEED = 0.20  # This is the lowest number we can do
 CTLR_LIGHT_UP_SLEEP_TIME = 0.25
 CLIENT_SOCK_SLEEP_TIME = 0.25
--- testing/gui/memory_in_game_screen.py
+++ testing/gui/memory_in_game_screen.py
@@ -22,6 +22,7 @@ class GameThread(QThread):
         
         # Show game over layout when signaled
         def on_game_over():
+            self.memory_game.disconnect_bluetooth()
             self.game_over.emit()
         
         # Run Memory Game
