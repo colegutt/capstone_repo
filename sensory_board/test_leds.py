21e2432bc8500482adfee476ceeefce1625d8c4c
colegutt <cole.gutterman@uky.edu>
2024-11-18 11:17:15 -0500

    Edited LED test script

--- sensory_board/test_leds.py
+++ sensory_board/test_leds.py
@@ -22,13 +22,13 @@ np_array = list(range(num_leds+1))
 # Create slice objects corresponding to the LED indices for each area
 #   and give those ranges of your LED strand meaningful names
 
-star_slice = slice(0, 30)
+star_slice = slice(0,5)
 star = np_array[star_slice]
 
-circle_slice = slice(0,30)
+circle_slice = slice(5,11)
 circle = np_array[circle_slice]
 
-heart_slice = slice(0, 30)
+heart_slice = slice(11,17)
 heart = np_array[heart_slice]
 
 triangle_slice = slice(0,30)
@@ -56,27 +56,27 @@ def turn_on_all_leds(dimming_factor):
 
     pixels.show()
 
-def turn_on_test_strip(dimming_factor, color):
-    for p in star:
+def turn_on_shape(shape, color, dimming_factor):
+    for p in shape:
         pixels[p] = dim_color(color, dimming_factor) 
 
-def test_led_main(color):
+def test_all_leds_main(shape, color):
     try:
         while True:
             turn_off_leds()
-            turn_on_test_strip(0.1, color)
+            turn_on_shape(shape, color, 0.1)
             time.sleep(1)
             turn_off_leds()
-            turn_on_test_strip(0.25, color)
+            turn_on_shape(shape, color, 0.25)
             time.sleep(1)
             turn_off_leds()
-            turn_on_test_strip(0.5, color)
+            turn_on_shape(shape, color, 0.5)
             time.sleep(1)
             turn_off_leds()
-            turn_on_test_strip(0.75, color)
+            turn_on_shape(shape, color, 0.75)
             time.sleep(1)
             turn_off_leds()
-            turn_on_test_strip(1, color)
+            turn_on_shape(shape, color, 1)
             time.sleep(1)
             turn_off_leds()
     except KeyboardInterrupt:
@@ -99,6 +99,12 @@ def turn_off_leds():
 
     pixels.show()
 
+def turn_off_shape(shape):
+    for p in square:
+        pixels[p] = (0, 0, 0)  # Turn off (black)
+    pixels.show()
+
+
 green_square_color = (0, 255, 0)
 blue_cloud_color = (0, 0, 255)
 purple_triangle_color = (128, 0, 128)
@@ -106,4 +112,44 @@ red_heart_color = (255, 0, 0)
 orange_circle_color = (255, 100, 0)
 yellow_star_color = (255, 165, 0)
 
-test_led_main(blue_cloud_color)
+
+def slowly_turn_on_shapes():
+    try:
+        turn_on_shape(heart, red_heart_color, 1)
+        time.sleep(1)
+        turn_on_shape(circle, orange_circle_color, 1)
+        time.sleep(1)
+        turn_on_shape(star, yellow_star_color, 1)
+    except KeyboardInterrupt:
+        turn_off_leds()
+
+def demo():
+    try:
+        while True:
+            turn_on_shape(heart, red_heart_color, 1)
+            time.sleep(0.5)
+            turn_on_shape(circle, orange_circle_color, 1)
+            time.sleep(0.5)
+            turn_on_shape(star, yellow_star_color, 1)
+            time.sleep(0.5)
+            turn_off_leds()
+
+            for i in range(0,10):
+                turn_on_shape(heart, red_heart_color, 1)
+                time.sleep(0.1)
+                turn_off_shape(heart)
+                turn_on_shape(circle, orange_circle_color, 1)
+                time.sleep(0.1)
+                turn_off_shape(circle)
+                turn_on_shape(star, yellow_star_color, 1)
+                time.sleep(0.1)
+                turn_off_shape(star)
+                turn_on_shape(circle, orange_circle_color, 1)
+                time.sleep(0.1)
+                turn_off_shape(circle)
+    except KeyboardInterrupt:
+        turn_off_leds()
+
+
+# pattern()
+# test_all_leds_main(star, yellow_star_color)
